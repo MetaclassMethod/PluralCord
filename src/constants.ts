eec039d3be1453116a5ec450c64d5963efc483af
Meta <seb.esposito@outlook.com>
2026-09-09 19:36:36 +1000

    Fix repo upstream

--- src/constants.ts
+++ src/constants.ts
@@ -3,4 +3,4 @@
  */
 
 export const PLUGIN_NAME = "PluralCord";
-export const REPO_URL = "https://github.com/MetaclassMethod/pluralgrace";
+export const REPO_URL = "https://github.com/MetaclassMethod/PluralCord";
